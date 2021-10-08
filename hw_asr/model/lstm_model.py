271c226c23cc1be245f2d5c21f465cc1b9aae0e3
ainmukh <aynurmukh@gmail.com>
2021-10-08 22:01:20 +0300

    lstm baseline

--- hw_asr/model/lstm_model.py
+++ hw_asr/model/lstm_model.py
@@ -7,7 +7,7 @@ class LSTMModel(BaseModel):
     def __init__(self, n_feats, n_class, fc_hidden=512, *args, **kwargs):
         super().__init__(n_feats, n_class, *args, **kwargs)
         self.encoder = nn.LSTM(n_feats, fc_hidden, batch_first=True, bidirectional=True, num_layers=2)
-        self.head = nn.Linear(fc_hidden, n_class)
+        self.head = nn.Linear(fc_hidden * 2, n_class)
 
     def forward(self, spectrogram, *args, **kwargs):
         x, _ = self.encoder(spectrogram.transpose(1, 2))
